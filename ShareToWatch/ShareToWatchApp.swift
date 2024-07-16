e2983d12b9af9ce5c4fed9d0718530812c20bad4
Denis <udevwork@gmail.com>
2024-07-16 10:20:13 +0200

    Initial Commit

---
+++ ShareToWatch/ShareToWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareToWatchApp.swift
+//  ShareToWatch
+//
+//  Created by Denis Kotelnikov on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShareToWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
